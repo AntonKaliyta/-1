4b1d6f13028b9196269f53cd2ec78b79771d5eac
«anton» <kaluta99@gmail.com>
2023-02-09 08:31:21 +0700

    нахождение квадрата числа

---
+++ задание1/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число ");
+// Варианты написания: 2 либо 3 строка
+//int number = Convert.ToInt32(Console.ReadLine());
+int number = int.Parse(Console.ReadLine());
+
+int sqr = number * number;
+// Варианты написания 8 либо 9 строка
+// System.Console.Write("Квадрат числа " + number + "равен " + sqr);
+System.Console.Write($"Квадрат числа {number} равен {sqr}");
+
+// можно сразу в обход 6 строки написать System.Console.Write($"Квадрат числа {number*number} ");
