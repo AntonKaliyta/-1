371cb7fd4a61d4769101db48f41e5f94f8c5428c
«anton» <kaluta99@gmail.com>
2023-02-10 19:22:31 +0700

    проверка является ли число корнем другого числа

---
+++ задание2/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Введите число");
+int number1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите квадрат числа");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 * number1 == number2)
+{
+    System.Console.WriteLine($"{number1} является корнем числа {number2}");
+}
+else
+{
+    System.Console.WriteLine($"{number1} не является корнем числа {number2}");
+}
